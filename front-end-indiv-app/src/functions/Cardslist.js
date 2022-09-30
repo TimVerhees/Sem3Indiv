cd0803e15d207a79e84dc48192db06e72e2dbb30
Verhees,Tim T.J.H <t.verhees@student.fontys.nl>
2022-09-30 16:15:43 +0200

    Changed some things in accordance to the front end, updated "UPDATE" method to not create an additional entry.

---
+++ front-end-indiv-app/src/functions/Cardslist.js
@@ -0,0 +1,6 @@
+export default function Carslist(props) {
+    console.log(props.card);
+    return(
+         <>
+         </>)
+    }
---
+++ front-end-indiv-app/src/functions/Parent.js
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react';
+import Axios from 'axios';
+
+export default function Parent() { 
+    //get data from API
+    const url = "http://localhost:8080/";
+
+    const [cards, getCards] = useState(' ');
+
+    useEffect( () => {
+        getAllCards();
+    }, []);
+
+    const getAllCards = () => {
+        Axios.get(`http://localhost:8080/cards`)
+        .then((response) => {
+            const allCards = response.data.cards.allCards;
+            getCards(allCards);
+        })
+        .catch(error => console.error('Error: ', { error}));
+    }
+        return(
+            <NoteTimeline cards= {cards}/>
+        )
+
+} 
